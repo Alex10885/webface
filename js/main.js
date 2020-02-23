64b5d22fb64b4df497c50ed939a68b6b1d89b77f
Alex10885 <aledor10885@gmail.com>
2020-02-23 04:05:03 +0300

    вебформа для курсов

---
+++ js/main.js
@@ -0,0 +1,116 @@
+let extrasensoryThree = document.getElementById('extrasensory_3');
+let russianRunes = document.getElementById('russian_runes');
+let graphicMagic = document.getElementById('graphic_magic');
+let extrasensTwo = document.getElementById('extrasens_2');
+let mandalas = document.getElementById('mandalas');
+let tarotMagic = document.getElementById('tarot_magic');
+let ifrites = document.getElementById('ifrites');
+let whiteShamanismс = document.getElementById('white_shamanismс');
+let bioenergyTwo = document.getElementById('bioenergy_2');
+let extrasensoryOne = document.getElementById('extrasensory_1');
+let pendulum = document.getElementById('pendulum');
+let runes = document.getElementById('runes');
+let taro = document.getElementById('taro');
+let playingCards = document.getElementById('playing_cards');
+let genus = document.getElementById('genus');
+let moneyMagic = document.getElementById('money');
+let blackShamanism = document.getElementById('black_shamanism');
+let battleMagic = document.getElementById('battle_magic');
+let sexMagic = document.getElementById('sex_magic');
+let tantraMagic = document.getElementById('tantra_magic');
+let villageMagic = document.getElementById('village_magic');
+let witchCraft = document.getElementById('witchcraft');
+let bioenergyOne = document.getElementById('bioenergy_one');
+let candleCastingMagic = document.getElementById('candle_casting_magic');
+
+extrasensoryThree.addEventListener('click', function(){
+    window.open('/404.html');   
+});
+
+russianRunes.addEventListener('click', function(){
+    window.open('/404.html');   
+});
+
+graphicMagic.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?section=album_3&w=product-191245025_3494887%2Fquery');
+});
+
+extrasensTwo.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+mandalas.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+tarotMagic.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+ifrites.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+white_shamanismс.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?section=album_6&w=product-191245025_3547993%2Fquery');
+});
+
+bioenergyTwo.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?section=album_2&w=product-191245025_3494369%2Fquery');
+});
+
+extrasensoryOne.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?section=album_2&w=product-191245025_3551994%2Fquery');
+});
+
+runes.addEventListener('click', function(){
+    window.open('/404.html');   
+});
+
+taro.addEventListener('click', function(){
+    window.open('/404.html'); 
+});
+
+playingCards.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?w=product-191245025_3496910%2Fquery');
+});
+
+genus.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+moneyMagic.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+blackShamanism.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+battleMagic.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+sexMagic.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+tantra_magic.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?w=product-191245025_3519435%2Fquery');
+});
+
+villageMagic.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?w=product-191245025_3496350%2Fquery');
+});
+
+witchCraft.addEventListener('click', function(){
+    window.open('/404.html');
+});
+
+bioenergyOne.addEventListener('click', function(){
+    window.open('https://vk.com/market-191245025?w=product-191245025_3493021%2Fquery');
+});
+
+candleCastingMagic.addEventListener('click', function(){
+    window.open('/404.html');
+});
